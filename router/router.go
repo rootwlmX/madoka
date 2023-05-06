2d555528fa40cf736c1e9b81c21b2d86cc3c6a63
Arcueid <1170895958@qq.com>
2023-05-06 15:17:31 +0800

    :sparkles:feat: 路由注册

--- router/router.go
+++ router/router.go
@@ -11,4 +11,5 @@ func InitRouter(engine *gin.Engine) {
 	new(v1.ArticleHandler).RegisterRouter(engine)
 	new(v1.CommentHandler).RegisterRouter(engine)
 	new(v1.ArticleCateHandler).RegisterRouter(engine)
+	new(v1.LoveHandler).RegisterRouter(engine)
 }
