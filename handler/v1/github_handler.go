7575aaef8df2bca122323d684ade23bfaa61d732
Arcueid <1170895958@qq.com>
2023-05-08 16:20:57 +0800

    fix: marshal check

--- handler/v1/github_handler.go
+++ handler/v1/github_handler.go
@@ -7,6 +7,7 @@ import (
 	"madoka/common"
 	"madoka/util"
 	"net/http"
+	"net/url"
 )
 
 // GithubHandler Github三方登录handler
@@ -16,6 +17,7 @@ type GithubHandler struct {
 const clientID string = "d36422ab56e8ee83ebd0"
 const clientSecret string = "915bbef9ce4a9ba04a4304fdcb5b707cd37bff41"
 const githubOauthBaseURL string = "https://github.com/login/oauth/access_token"
+const redirectURL string = "http://madokami.top/v1/oauth/github/redirect"
 const state string = "blog"
 
 // RegisterRouter 路由注册
@@ -27,8 +29,14 @@ func (h *GithubHandler) RegisterRouter(engine *gin.Engine) {
 // GetGithubToken Github重定向handler
 func (h *GithubHandler) GetGithubToken(c *gin.Context) {
 	code := c.Query("code")
-	request := fmt.Sprintf("%s?client_id=%s&client_secret=%s&code=%s&state=%s", githubOauthBaseURL, clientID, clientSecret, code, state)
-	response, err := http.Post(request, "application/json", nil)
+	loginUrl := fmt.Sprintf("%s?client_id=%s&client_secret=%s&code=%s&state=%s", githubOauthBaseURL, clientID, clientSecret, code, state)
+	response, err := http.PostForm(loginUrl, url.Values{
+		"client_id":     {clientID},
+		"client_secret": {clientSecret},
+		"redirect_uri":  {redirectURL},
+		"code":          {code},
+		"state":         {state},
+	})
 	if err != nil {
 		common.Failed(c, err.Error())
 		return
@@ -36,7 +44,6 @@ func (h *GithubHandler) GetGithubToken(c *gin.Context) {
 	defer response.Body.Close()
 	bs, _ := io.ReadAll(response.Body)
 	body := string(bs)
-	fmt.Println(body)
 	resultMap := util.JSONToMap(body)
 	accessToken := resultMap["access_token"].(string)
 	getGithubUserMessage(accessToken, c)
