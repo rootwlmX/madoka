65beedb3fc53eda85d6db8a5c4011db20035388a
Arcueid <1170895958@qq.com>
2023-05-11 14:36:56 +0800

    feat: like and collect count

--- models/xorm_model.go
+++ models/xorm_model.go
@@ -5,12 +5,14 @@ import "time"
 
 // Article 文章实体结构
 type Article struct {
-	ID         int       `json:"id" xorm:"id pk autoincr notnull"`
-	Title      string    `json:"title" xorm:"title"`
-	Content    string    `json:"content" xorm:"content LONGTEXT"`
-	ClassID    int       `json:"classId" xorm:"class_id"`
-	CreateTime time.Time `json:"createTime" xorm:"create_time"`
-	UpdateTime time.Time `json:"updateTime" xorm:"update_time"`
+	ID           int       `json:"id" xorm:"id pk autoincr notnull"`
+	Title        string    `json:"title" xorm:"title"`
+	Content      string    `json:"content" xorm:"content LONGTEXT"`
+	ClassID      int       `json:"classId" xorm:"class_id"`
+	CreateTime   time.Time `json:"createTime" xorm:"create_time"`
+	UpdateTime   time.Time `json:"updateTime" xorm:"update_time"`
+	LikeCount    int64     `json:"likeCount" xorm:"like_count"`
+	CollectCount int64     `json:"collectCount" xorm:"collect_count"`
 }
 
 // Comment 评论实体结构
