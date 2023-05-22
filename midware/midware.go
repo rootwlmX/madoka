7e0a761c07dd0f0c101bd75da53efbf1fa6a191c
Arcueid <1170895958@qq.com>
2023-05-22 09:33:55 +0800

    feat: fix lint

--- midware/midware.go
+++ midware/midware.go
@@ -1,3 +1,4 @@
+// Package midware 中间件
 package midware
 
 import (
@@ -5,6 +6,7 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+// BrowseMiddleWare 浏览量
 func BrowseMiddleWare() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		fmt.Println("success")
